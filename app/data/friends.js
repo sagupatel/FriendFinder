bf22912d9b92616f95310c9e40edc6e049c3198f
SagarPatel <sagaroceanp@gmail.com>
2019-08-14 09:24:55 -0500

    files added

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+var friendsArray = [
+	{
+		name: 'Bebop',
+	},
+	{
+		name: 'Rocksteady',
+	},
+	{
+		name: 'Splinter',
+	},
+	{
+		name: 'Shredder',
+	},
+	{
+		name: 'Vanilla Ice',
+	},
+	{
+		name: 'Donatello',
+	},
+	{
+		name: 'Raphael',
+	},
+	{
+		name: 'Michelangelo',
+	},
+	{
+		name: 'Leonardo',
+];
+
+module.exports = friendsArray;
